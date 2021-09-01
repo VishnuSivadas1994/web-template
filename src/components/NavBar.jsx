8501137e39416955ad329cb95091b4ede111f056
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-09-01 12:10:31 -0400

    navbar with social media icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.styles.scss';
+import { SocialIcon } from 'react-social-icons';
 
 const NavBar = () => {
     return (
-        
+
         <div className='header'>
             <div>
                 <NavLink to="/">
@@ -12,21 +13,27 @@ const NavBar = () => {
                 </NavLink>
             </div>
             <div className='options'>
-                <NavLink className='option' to="/" exact>
+                <NavLink className='option' to="/" exact> {/*activeClassName*/}
                     HOME
-                    </NavLink>
+                </NavLink>
 
                 <NavLink className='option' to="/post">
                     POST
-                    </NavLink>
+                </NavLink>
 
                 <NavLink className='option' to="/project">
                     PROJECT
-                    </NavLink>
+                </NavLink>
 
                 <NavLink className='option' to="/about">
                     ABOUT
-                    </NavLink>
+                </NavLink>
+            </div>
+            <div className='options2'>
+                <SocialIcon url="https://github.com/VishnuSivadas1994" className="option" target="_blank" fgColor="#fff" stlye={{ height: 25, width: 25 }} />
+                <SocialIcon url="https://www.linkedin.com/in/sivadasv/" className="option" target="_blank" fgColor="#fff" stlye={{ height: 25, width: 25 }} />
+                <SocialIcon url="#" className="option" target="_blank" fgColor="#fff" stlye={{ height: 25, width: 25 }} />
+
             </div>
         </div>
     )
